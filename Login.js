5f724172a02b1b1c2dabb346c148514887258874
SeguRuiz <ruizsegura96199@gmail.com>
2024-05-08 15:53:42 -0600

    login y register funcionales

--- Login.js
+++ Login.js
@@ -2,10 +2,21 @@ let formLog = document.getElementById('formLog')
 
 formLog.addEventListener('submit', (quedate) => {
 quedate.preventDefault()
-
 let correo = document.getElementById('inputsLemail').value
 let password = document.getElementById('inputsLpassword').value
 
-let listaLogs = 
+
+let listaL = {
+correo: correo.value,
+password: password.value
+}
+let usuarios = JSON.parse(localStorage.getItem('usuarios'))
+let cuentas = JSON.parse(localStorage.getItem('cuentas'))
+let buscador = cuentas.find(busca => busca.email == correo && busca.password == password)
+if (buscador) {
+   localStorage.setItem('usuarios', JSON.stringify(buscador))
+} else {
+    console.log('La cuenta es incorrecta')
+}
 
 })
