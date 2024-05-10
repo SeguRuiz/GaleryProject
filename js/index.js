53cb2171fdce74b5b6b7a9467ec02038ac01f1dd
SeguRuiz <ruizsegura96199@gmail.com>
2024-05-09 22:36:06 -0600

    cambio al home

--- js/index.js
+++ js/index.js
@@ -1,9 +1,103 @@
-let logOut = document.getElementById("LogOut");
-let input = document.getElementById("distintivo");
-let btn = document.getElementById("btn");
-let bienvenida = document.getElementById("bienvenida");
+let log = document.getElementById('logeate')
 
-let texto = document.createElement("h1");
+log.addEventListener('click', ()=>{
+  window.location.href = 'http://127.0.0.1:5500/Login.html'
+})
+
+
+//botones log out y registrate
+let logOut = document.getElementById("logOut");
+
+let registrate = document.getElementById('registrarse')
+
+
+//contenedor de las imagenes
+let imagenes = document.getElementById("imagenes");
+// llama mi puerta a mi sesion
+let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+// establece una puerta de datos leibles por mi de nombre == al que esta sesion
+let imgs = JSON.parse(localStorage.getItem(usuarios.nombre)) || []
+//Bienvenida
+
+
+if ( usuarios != null) {
+  
+  log.innerHTML = ''
+  
+}
+
+document.getElementById('bienvenida').innerHTML = 'Bienvenid@ ' + usuarios.nombre + ' ¿que imagen subiras hoy?'
+
+
+
+
+
+
+
+
+//log out funcion
+logOut.addEventListener('click', ()=>{
+
+window.location.reload()
+localStorage.removeItem('usuarios')
+
+window.location.href = 'http://127.0.0.1:5500/Login.html'
+
+})
+
+
+//borra el login si usuarios != null
+
+
+
+//revisa datos de imagen
+for (let m = 0; m < imgs.length; m++) {
+  const revisaImgs = imgs[m].imgs;
+
+
+
+
+  let div = document.createElement('div')
+  let img = document.createElement('img')
+  img.src = revisaImgs
+  img.classList.add('img')
+  div.classList.add('divImg')
+
+  imagenes.appendChild(div)
+  div.appendChild(img)
+
+  
+}
+//crea elemento automatico
+
+//guarda imagenes
+let inputTitulo = document.getElementById("inputTitulo");
+
+inputTitulo.addEventListener("change", (x) => {
+
+  
+  let archivo = inputTitulo.files[0];
+
+  let leelo = new FileReader();
+
+  leelo.addEventListener("load", () => {
+    let img = {
+      usuario: usuarios.nombre,
+      imgs: leelo.result,
+    };
+    
+
+    
+    imgs.push(img);
+    localStorage.setItem(usuarios.nombre, JSON.stringify(imgs));
+    window.location.reload()
+  });
+
+  leelo.readAsDataURL(archivo);
+});
+
+//agarra archivos del local storage
+
 
 
 
